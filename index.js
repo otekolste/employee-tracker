e5e523a9115f2eb3e56aadf45434d8a390f326cd
otekolste <58699635+otekolste@users.noreply.github.com>
2024-08-16 17:44:16 -0400

    cleaned up code, added view prompts to inquirer

--- index.js
+++ index.js
@@ -7,27 +7,52 @@ db.query('SELECT * FROM department', function (err, {rows}) {
     console.log(rows);
   });
   */
-
-  inquirer
-    .prompt([
-      {
-        type: 'list',
-        message: 'Welcome to the Employee Tracker. What would you like to do?',
-        name:'whatDo',
-        choices: ['View all departments','View all roles','View all employees','Add a department','Add a role','Add an employee','Update an employee role']
-      },
-      {
-        when: (response) => {
-          if(response.whatDo==='View all departments') {
-            db.query('SELECT name FROM department', function (err, {rows}) {
-              console.log(rows);
-            });
-            return;
-          }
-        }
+ 
+const main = async () => {
+  let quit = false;
+  while(!quit) {
+    await inquirer
+    .prompt(menu)
+    .then((response) => {
+      if(response.whatDo == 'Quit') {
+        quit = true;
       }
+      else{
+        handleUserInput(response);
+      }
+    })
+  }
+  process.exit();
+}
 
-    ])
+const handleUserInput = async (input) => {
+  switch(input.whatDo) {
+    case 'View all departments':
+      await db.query('SELECT * FROM department', function (err, {rows}) {
+        console.log(rows);
+      });
+      break;
+    case 'View all roles':
+      await db.query('SELECT * FROM role', function (err, {rows}) {
+        console.log(rows);
+      });
+    case 'View all employees':
+      await db.query('SELECT * FROM employee', function (err, {rows}) {
+        console.log(rows);
+      });
+  }
+}
+
+const menu = 
+  {
+    type: 'list',
+    message: 'Welcome to the Employee Tracker. What would you like to do?',
+    name:'whatDo',
+    choices: ['View all departments','View all roles','View all employees','Add a department','Add a role','Add an employee','Update an employee role','Quit']
+  }
+
+await main();
+
 
 
 
